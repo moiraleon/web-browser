b01046733f06cb9ae730c6cf864d655b5ab49008
Moira Leon <moira.leon2@gmail.com>
2021-10-01 16:29:53 -0700

    initial commit

---
+++ src/test/java/com/web/browser/BrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.browser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
